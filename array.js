2de9b7d4e3b6ea0d6c39a1b25b403fd54bcef43a
22360013007 <mudiyamkavitha@gmail.com>
2025-02-06 12:58:12 +0530

    example programs

---
+++ array.js
@@ -0,0 +1,33 @@
+//let arr=[1,34,6,78];
+//let newarr=arr.join()
+//console.log(typeof(newarr))
+//console.log(newarr)
+//console.log (arr.indexOf(1))
+//arr.push(7)
+//console.log (arr)
+//arr.pop()
+//console.log(arr)
+//arr.unshift(809)
+//arr.shift()
+//console.log(arr)
+
+
+let arr=[1,3,5,87];
+console.log(arr)
+let newarr1=arr.splice(0,3)
+console.log(newarr1)
+
+const myArr = [0,1,2,3,4,5]
+console.log("S",myArr);
+
+const myn1=myArr.slice(1,3)
+
+console.log(myn1);
+console.log("A",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("M",myArr);
+console.log(myn2);
+
+
+
---
+++ functions.js
@@ -0,0 +1,9 @@
+//function name(){
+   // console.log("function got run")
+//}
+function add(a,b,c){
+    console.log(a+b+c)
+    return 'Welcome to Java Script'
+}
+let newvalue=add(8,18,9)
+console.log(newvalue)
